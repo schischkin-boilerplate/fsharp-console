fd52af940095c8ea3f98695264960989304be42e
Daniel Schischkin <daniel@schischkin.info>
2020-10-01 16:44:27 +0200

    Adjust entrypoint.sh by adding dependencies and some corrections

--- build.entrypoint.sh
+++ build.entrypoint.sh
@@ -10,8 +10,13 @@ dotnet new sln -o . -n ${PROJECT_NAME} --force
 
 echo "Creating projects.."
 dotnet new console -o ${PROJECT_NAME} -n ${PROJECT_NAME} --language="F#"
-dotnet new xunit -o ${PROJECT_NAME}.Tests -n ${PROJECT_NAME} --language="F#"
-dotnet add reference ${PROJECT_NAME}.Tests ${PROJECT_NAME}
+dotnet new xunit -o ${PROJECT_NAME}.Tests -n Test --language="F#"
+dotnet sln add ${PROJECT_NAME}
+dotnet sln add ${PROJECT_NAME}.Tests
+dotnet add ${PROJECT_NAME}.Tests reference ${PROJECT_NAME}
 
 echo "Installing dependencies.."
-dotnet add package Logary --version 5.0.1
+dotnet add ${PROJECT_NAME} package Logary
+dotnet add ${PROJECT_NAME}.Tests package FsUnit.xUnit
+
+echo "Done"
